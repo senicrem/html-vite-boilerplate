5d4e79bb08d4b3cbf57f2eb66968804f5ba2aeb4
JohnJay <mercinesjohnjay@gmail.com>
2025-02-18 09:50:51 +0800

    add prettier

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { add, subtract } from './modules/math.js';
+import { add, subtract } from './modules/math.js'
 
-console.log(add(5, 3));       // 8
-console.log(subtract(5, 3));  // 2
+console.log(add(5, 3)) // 8
+console.log(subtract(5, 3)) // 2
